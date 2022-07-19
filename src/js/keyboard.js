6a182e26404a8007bb16aa1bd745d8131ab95a97
literate <gaekoon@gmail.com>
2022-07-19 10:39:06 +0900

    이벤트 적용 완료

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -5,6 +5,9 @@ export class Keyboard {
   #keyboardElement;
   #inputGroupElement;
   #inputElement;
+  #keyMouseElement;
+  #keyPress;
+  #mousePress;
 
   constructor() {
     this.#assignElement();
@@ -24,9 +27,11 @@ export class Keyboard {
   #addEvent() {
     this.#switchElement.addEventListener('change', this.#onChangeTheme);
     this.#fontSelectElement.addEventListener('change', this.#onChangeFont);
-    document.addEventListener('keydown', this.#onKeydown.bind(this));
+    document.addEventListener('keydown', this.#onKeydown);
     document.addEventListener('keyup', this.#onKeyup.bind(this));
     this.#inputElement.addEventListener('input', this.#onInput.bind(this));
+    document.addEventListener('mousedown', this.#onMouseDown.bind(this));
+    document.addEventListener('mouseup', this.#onMouseUp.bind(this));
   }
 
   #onChangeTheme(event) {
@@ -40,13 +45,22 @@ export class Keyboard {
     document.body.style.fontFamily = event.target.value;
   }
 
-  #onKeydown(event) {
-    console.log(this);
+  #onKeydown = (event) => {
+    if (this.#mousePress) return;
+    this.#keyPress = true;
     const key = this.#getKeyboardKey(event);
     key?.classList.add('active');
-  }
+  };
+
+  //   #onKeydown(event) {
+  //     console.log(this);
+  //     // const key = this.#getKeyboardKey(event);
+  //     // key?.classList.add('active');
+  //   }
 
   #onKeyup(event) {
+    if (this.#mousePress) return;
+    this.#keyPress = false;
     const key = this.#getKeyboardKey(event);
     key?.classList.remove('active');
   }
@@ -60,6 +74,33 @@ export class Keyboard {
     event.target.value = inputValue.replace(koreanRegex, '');
   }
 
+  #onMouseDown(event) {
+    if (this.#keyPress) return;
+    this.#mousePress = true;
+    // 해당 키 밖에서 mouseup을 했을 때를 위해 필드로 저장
+    this.#keyMouseElement = this.#getKeyFromMouse(event);
+    this.#keyMouseElement?.classList.add('active');
+  }
+
+  #onMouseUp() {
+    // const isActive = this.#keyMouseElement?.classList.contains('active');
+    // if (isActive) {
+
+    // }
+    if (this.#keyPress) return;
+    this.#mousePress = false;
+    const val = this.#keyMouseElement?.dataset.val;
+    if (!!val && val !== 'Space' && val !== 'Backspace') {
+      this.#inputElement.value += val;
+    } else if (val === 'Space') {
+      this.#inputElement.value += ' ';
+    } else if (val === 'Backspace') {
+      this.#inputElement.value = this.#inputElement.value.slice(0, -1); // (0, length - 1)
+    }
+
+    this.#keyMouseElement?.classList.remove('active');
+  }
+
   #getKeyboardKey(event) {
     return this.#keyboardElement.querySelector(`[data-code=${event.code}]`);
   }
@@ -67,5 +108,9 @@ export class Keyboard {
   #isKorean(inputValue) {
     const koreanRegex = /[ㄱ-ㅎㅏ-ㅣ가-힣]/;
     return koreanRegex.test(inputValue);
+  }
+
+  #getKeyFromMouse(event) {
+    return event.target.closest('div.key');
   }
 }
